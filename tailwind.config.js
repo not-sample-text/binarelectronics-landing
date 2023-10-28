504acdbd904c40c771a678e91ef8cb4f0df4dfd9
Pal Antonio Eduard <palantonio420@gmail.com>
2023-10-28 19:57:16 +0300

    putting something on the page, refining next time
    added card description text horizontal drag+icons
    
    need to change product icons
    
    final commit?
    
    squashed 4 commits

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["*.{html,css},!node_modules/*"],
+	theme: {
+		extend: {
+			colors: {
+				"gold": "#B97532",
+				"circuit-green": "#196433"
+			}
+		}
+	},
+	plugins: []
+};
